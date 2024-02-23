7fa0c18b31b1fa7ebee01dc1a3b97d2412d7152c
SeeuSim <seeusimong@gmail.com>
2024-02-23 10:53:21 +0800

    Add model config
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- aws-backend/sgmkr-ep-embed-image/main.py
+++ aws-backend/sgmkr-ep-embed-image/main.py
@@ -2,7 +2,10 @@ from transformers import AutoImageProcessor
 from PIL import Image
 import sys
 
-preprocessor = AutoImageProcessor.from_pretrained("facebook/convnextv2-large-22k-384")
+#model = "google/vit-base-patch16-224-in21k"
+model = "facebook/convnextv2-large-22k-384"
+
+preprocessor = AutoImageProcessor.from_pretrained(model)
 
 
 def get_embedding(image_path):
@@ -10,6 +13,7 @@ def get_embedding(image_path):
         image = Image.open(image_path).convert("RGB")
     except Exception as e:
         exit(1)
+    
     image = image.resize((384, 384))
     tensor = preprocessor(image, return_tensors="pt")
     print(tensor['pixel_values'].shape)
