2d079dc97b2783c41f3ff094696ee24ebf7b2f3c
JamesLiuZX <liuzixin@comp.nus.edu.sg>
2024-03-19 19:44:59 -0700

    Add JJ's settings page code

--- frontend/SettingsPage.jsx
+++ frontend/SettingsPage.jsx
@@ -1,6 +1,16 @@
 // This example is for guidance purposes. Copying it will come with caveats.
 
-import { BlockStack, Box, Card, InlineGrid, Page, Text, TextField, Button } from '@shopify/polaris';
+import {
+  BlockStack,
+  Box,
+  Card,
+  InlineGrid,
+  Page,
+  Text,
+  TextField,
+  Button,
+  Select,
+} from '@shopify/polaris';
 import { useAction, useFindOne } from '@gadgetinc/react';
 import { useState, useCallback, useEffect } from 'react';
 import { api } from './api';
@@ -15,6 +25,9 @@ const SettingsPage = () => {
     if (data) {
       setIntroductionMessage(data?.introductionMessage || '');
       setName(data?.name || '');
+      setSelectedPersonality(data?.personality || 'Friendly');
+      setSelectedRole(data?.role || 'Advisor');
+      setSelectedTalkativeness(data?.talkativeness || '1');
     }
   }, [data]);
 
@@ -24,7 +37,51 @@ const SettingsPage = () => {
     []
   );
 
-  const [{ loading, error }, updateChatbotSettings] = useAction(api.ChatbotSettings.update);
+  const [selectedPersonality, setSelectedPersonality] = useState('');
+
+  const handleSelectChange = useCallback((value) => setSelectedPersonality(value), []);
+
+  const personalityOptions = [
+    { label: 'Friendly', value: 'FRIENDLY' },
+    { label: 'Informative', value: 'INFORMATIVE' },
+    { label: 'Persuasive', value: 'PERSUASIVE' },
+    { label: 'Witty', value: 'WITTY' },
+    {
+      label: 'Empathetic',
+      value: 'EMPATHETIC',
+    },
+    {
+      label: 'Auto',
+      value: 'AUTO',
+    },
+  ];
+
+  const talkativenessOptions = [
+    { label: '1', value: '1' },
+    { label: '2', value: '2' },
+    { label: '3', value: '3' },
+    { label: '4', value: '4' },
+    { label: '5', value: '5' },
+  ];
+
+  const roleOptions = [
+    { label: 'Advisor', value: 'ADVISOR' },
+    { label: 'Enthusiast', value: 'ENTHUSIAST' },
+    { label: 'Professional', value: 'PROFESSIONAL' },
+    { label: 'Storyteller', value: 'STORYTELLER' },
+    { label: 'Concierge', value: 'CONCIERGE' },
+    { label: 'Auto', value: 'AUTO' },
+  ];
+
+  const [selectedRole, setSelectedRole] = useState('');
+
+  const handleRoleChange = useCallback((value) => setSelectedRole(value), []);
+
+  const [selectedTalkativeness, setSelectedTalkativeness] = useState('');
+
+  const handleTalkativenessChange = useCallback((value) => setSelectedTalkativeness(value), []);
+
+  const [{ loading }, updateChatbotSettings] = useAction(api.ChatbotSettings.update);
 
   const handleSave = async () => {
     try {
@@ -34,6 +91,9 @@ const SettingsPage = () => {
         id: data.id,
         name: name,
         introductionMessage: introductionMessage,
+        personality: selectedPersonality,
+        role: selectedRole,
+        talkativeness: selectedTalkativeness,
       });
       console.log(newTask);
       // Handle success (e.g., clear form, show message, etc.)
@@ -80,12 +140,39 @@ const SettingsPage = () => {
                 onChange={handleIntroductionMessageChange}
                 autoComplete='off'
               />
+
+              <Select
+                label='Personality'
+                options={personalityOptions}
+                onChange={handleSelectChange}
+                value={selectedPersonality}
+              />
+              <Select
+                label='Talkativeness'
+                options={talkativenessOptions}
+                onChange={handleTalkativenessChange}
+                value={selectedTalkativeness}
+              />
+              <Select
+                label='Roles'
+                options={roleOptions}
+                onChange={handleRoleChange}
+                value={selectedRole}
+              />
             </BlockStack>
           </Card>
+          <Box
+            as='section'
+            paddingInlineStart={{ xs: 400, sm: 0 }}
+            paddingInlineEnd={{ xs: 400, sm: 0 }}
+          >
+            <BlockStack gap='400' />
+          </Box>
+          <Button onClick={handleSave} disabled={loading}>
+            Update Chatbot Settings
+          </Button>
         </InlineGrid>
-        <Button onClick={handleSave} disabled={loading}>
-          Update Chatbot Settings
-        </Button>
+
         {/* {smUp ? <Divider /> : null} */}
         {/* <InlineGrid columns={{ xs: "1fr", md: "2fr 5fr" }} gap="400">
 			<Box
