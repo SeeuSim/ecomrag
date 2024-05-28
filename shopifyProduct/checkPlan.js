5e67797e4dbd21b7d0217d740d05460e9e96b09b
SeeuSim <seeusimong@gmail.com>
2024-05-28 22:26:45 +0800

    Add validation for image per product
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- shopifyProduct/checkPlan.js
+++ shopifyProduct/checkPlan.js
@@ -1,5 +1,5 @@
 import { Client } from '@gadget-client/ecomrag';
-import { logger as gadgetLogger, CreateShopifyProductActionContext } from 'gadget-server';
+import { logger as gadgetLogger } from 'gadget-server';
 import { PLAN_LIMITS } from '../plan/utils';
 
 /**
@@ -7,24 +7,17 @@ import { PLAN_LIMITS } from '../plan/utils';
  */
 
 /**
- * @typedef { CreateShopifyProductActionContext. } ShopifyProduct
+ * @typedef { Awaited<ReturnType<typeof Client.prototype.shopifyProduct.findOne>> } ShopifyProduct
  */
 
-/** @type { ({ record, api, logger }: { record: ShopifyProduct, api: typeof Client.prototype, logger: typeof gadgetLogger }) => Promise<boolean>} */
-export const tryIncrShopSyncCount = async ({ record, api, logger }) => {
+/** @type { ({ record, api, logger, isUpdate }: { record: ShopifyProduct, api: typeof Client.prototype, logger: typeof gadgetLogger, isUpdate?: boolean }) => Promise<boolean>} */
+export const tryIncrProductSyncCount = async ({ record, api, logger, isUpdate }) => {
   const plan = await api.plan.findByShop(record.shopId);
   const { tier } = /**@type { Plan } */ (plan);
   const limit = PLAN_LIMITS[tier].productSyncCount;
   const withinLimit = plan.productSyncCount < limit;
 
-  // Only proceed if the product count is within the limit
-  // only run if the product does not have an embedding, or if the title or body have changed
-  //logger.info({ productCount: productSyncCount, productLimit }, 'product count and limit');
-  if (
-    (withinLimit && !record.descriptionEmbedding) ||
-    record.changed('title') ||
-    record.changed('body')
-  ) {
+  if (withinLimit || isUpdate) {
     try {
       if (withinLimit) {
         await api.internal.plan.update(plan.id, {
@@ -37,11 +30,14 @@ export const tryIncrShopSyncCount = async ({ record, api, logger }) => {
       }
       return true;
     } catch (error) {
-      logger.error({ error }, 'Error incrementing Sync Count');
+      const { name, message, stack, cause } = /**@type { Error } */ (error);
+      logger.error({ name, message, stack, cause }, 'Error incrementing Sync Count');
       return false;
     }
   } else {
-    logger.info('Product limit reached for the current plan. Skipping embedding creation.');
+    if (!withinLimit) {
+      logger.info('Product limit reached for the current plan. Skipping embedding creation.');
+    }
     return false;
   }
 };
